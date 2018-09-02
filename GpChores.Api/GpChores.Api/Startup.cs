7b5a25620df24417325d997ef681c29cbe3832a2
marvinpjr <marvin@marvinpalmer.com>
2018-09-01 20:24:41 -0500

    making a more specific version number

--- GpChores.Api/GpChores.Api/Startup.cs
+++ GpChores.Api/GpChores.Api/Startup.cs
@@ -24,7 +24,7 @@ namespace GpJobs.Api
             services.AddTransient(typeof(IDataService<>),typeof(DataService<>));
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "GP jobs API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "GP jobs API", Version = "v1.0.0.1" });
             });
         }
 
